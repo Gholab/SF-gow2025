881ff6ba8abe0dc6d86fac6566ad3b9d6bf49ec9
Gholab <hajarelgholabzouri@gmail.com>
2024-12-30 20:14:33 +0100

    normal setup

---
+++ src/components/BasicScene.vue
@@ -0,0 +1,29 @@
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { FirstScene } from '../Scenes/FirstScene';
+onMounted(()=>
+{
+  const canvas = document.querySelector('canvas');
+  if(canvas === null)
+  {
+    throw new Error('Canvas not found');
+  }
+  new FirstScene(canvas);
+})
+</script>
+
+<template>
+  <div>
+    <canvas></canvas>
+  </div>
+</template>
+
+<style scoped>
+canvas {
+  width: 100%;
+  height: 100%;
+  display: block; 
+}
+
+</style>
